28aeab5fabe1541b7a621b537ec7a77735f7ccd8
Khaleelur <khaleelrrahman2002@gmail.com>
2023-10-13 12:26:29 +0800

    Add tests for Tag class

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,8 +1,18 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTags.VENUES;
+import static seedu.address.testutil.TypicalTags.EVENT_PLANNER;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TagBuilder;
 
 public class TagTest {
 
@@ -18,9 +28,35 @@ public class TagTest {
     }
 
     @Test
-    public void isValidTagName() {
+    public void isValidTagName_nullTag() {
         // null tag name
-        assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+        assertThrows(NullPointerException.class, () -> Tag.isValidTagName("vendor"));
+    }
+    @Test
+    public void isValidTagName_nonNullTag() {
+        String expected = "[vendor]";
+        assertEquals(expected, new Tag("vendor").toString());
+    }
+
+     @Test
+    public void isSameTag() {
+        // same object -> returns true
+        assertTrue(VENUES.isSameTag(VENUES));
+
+        // null -> returns false
+        assertFalse(VENUES.isSameTag(null));
+
+        // Tag name differs in case -> returns false
+        Tag editedVenues = new TagBuilder().withTag(VENUES.getTagName().toUpperCase()).build();
+        assertFalse(VENUES.isSameTag(editedVenues));
+
+        // Tag name has trailing spaces -> throws illegalArgumentException as " " found
+        String tagNameWithTrailingSpaces = VENUES.getTagName() + " ";
+        assertThrows(IllegalArgumentException.class, () -> new TagBuilder().withTag(tagNameWithTrailingSpaces).build());
+
+        // Tag name has non-alphanumeric character -> throws illegalArgumentException
+         String tagNameWithNonAlphanumericChar = VENUES.getTagName() + "_";
+         assertThrows(IllegalArgumentException.class, () -> new TagBuilder().withTag(tagNameWithNonAlphanumericChar).build());
     }
 
 }
