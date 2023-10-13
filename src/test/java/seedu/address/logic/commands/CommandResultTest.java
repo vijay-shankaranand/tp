923432c3fc248df20615bba0c5be52408e224491
tjch-o <tzejie.c@gmail.com>
2023-10-13 20:48:23 +0800

    Fix stylecheck errors

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.swing.text.View;
-
 import org.junit.jupiter.api.Test;
 
 public class CommandResultTest {
