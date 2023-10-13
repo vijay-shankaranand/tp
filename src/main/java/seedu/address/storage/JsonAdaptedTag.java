6fecb75309a5ede54f567bc1848f44662dcd9e60
Khaleelur <khaleelrrahman2002@gmail.com>
2023-10-13 17:32:02 +0800

    Add test cases for ModelManager class

--- src/main/java/seedu/address/storage/JsonAdaptedTag.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTag.java
@@ -1,7 +1,6 @@
 package seedu.address.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -18,7 +17,7 @@ class JsonAdaptedTag {
      * Constructs a {@code JsonAdaptedTag} with the given {@code tagName}.
      */
     @JsonCreator
-    public JsonAdaptedTag(@JsonProperty("tag") String tagName) {
+    public JsonAdaptedTag(String tagName) {
         this.tagName = tagName;
     }
 
