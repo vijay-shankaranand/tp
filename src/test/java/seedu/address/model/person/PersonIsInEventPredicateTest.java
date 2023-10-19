bd6f3371a06cdb0d2f6bbaf86a9bbddbabebb41f
rionshocker <jaryl591@gmail.com>
2023-10-20 03:00:51 +0800

    Add additional tests

--- src/test/java/seedu/address/model/person/PersonIsInEventPredicateTest.java
+++ src/test/java/seedu/address/model/person/PersonIsInEventPredicateTest.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.EventAddress;
 import seedu.address.model.event.EventDate;
@@ -38,5 +39,13 @@ public class PersonIsInEventPredicateTest {
 
         // different event -> returns false
         assertFalse(firstPredicate.equals(secondPredicate));
+    }
+
+    @Test
+    public void toString_testEquals() {
+        Event firstEvent = new Event(new EventName("Job Fest 2023"), new EventDate("2023-12-23"),
+            new EventAddress("NUS"), new HashSet<Person>());
+        PersonIsInEventPredicate firstPredicate = new PersonIsInEventPredicate(firstEvent);
+        String expected = new ToStringBuilder(firstPredicate).add("event", firstEvent).toString();
     }
 }
