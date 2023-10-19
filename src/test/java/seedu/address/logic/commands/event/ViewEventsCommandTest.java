ad50f41a9806f4c4d57129f7bf4db8c87e8c5873
tjch-o <tzejie.c@gmail.com>
2023-10-20 00:41:03 +0800

    Add test cases for view_events

---
+++ src/test/java/seedu/address/logic/commands/event/ViewEventsCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands.event;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+
+public class ViewEventsCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ViewEventsCommand(), model, ViewEventsCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ViewEventsCommand(), model, ViewEventsCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
