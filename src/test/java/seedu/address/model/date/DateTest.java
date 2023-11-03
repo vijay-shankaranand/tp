57e3dfea862ac65da8f1dd8e94e1a65b435ef747
tjch-o <tzejie.c@gmail.com>
2023-11-04 02:01:20 +0800

    Refactor code

--- src/test/java/seedu/address/model/date/DateTest.java
+++ src/test/java/seedu/address/model/date/DateTest.java
@@ -2,11 +2,27 @@ package seedu.address.model.date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
 public class DateTest {
+    @Test
+    public void isWithinThreeDays_returnsTrue() {
+        Date futureDate = new Date(LocalDate.now().plusDays(2).toString());
+        assertEquals(futureDate.isWithinThreeDays(), true);
+
+        Date pastDate = new Date(LocalDate.now().minusDays(2).toString());
+        assertEquals(pastDate.isWithinThreeDays(), false);
+
+        Date todayDate = new Date(LocalDate.now().toString());
+        assertEquals(todayDate.isWithinThreeDays(), true);
+    }
+
     @Test
     public void equals() {
         LocalDate nullDate = null;
-        LocalDate date = new Date("2023-11-04");
+        Date date = new Date("2023-11-04");
         assertEquals(date.equals(nullDate), false);
     }
 }
